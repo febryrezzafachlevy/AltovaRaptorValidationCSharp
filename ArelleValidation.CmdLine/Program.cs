d0aa7f8d5f630cbf9d93318765e98aa80d9f0c7e
Febry Rezza Fachlevy <febry.fachlevy@isidsea.com>
2023-08-11 16:26:20 +0700

    Initial

---
+++ ArelleValidation.CmdLine/Program.cs
@@ -0,0 +1,40 @@
+﻿using ArelleValidation.CmdLine.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArelleValidation.CmdLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arellePath = "\"D:\\SYSTEM\\Windows\\Program Files\\Arelle\\arelleCmdLine.exe\"";
+            var instancePath = @"E:\WORK\PROJECT\MALEO\PRODUCTION\SIPP\1_VERSION_5_1\WEB\App_Data\INSTANCE\";
+            var instanceName = "260460-2022-04-30-220020300";
+
+            var arelleArgument = "--file " + "\"{INSTANCENAME}.xml\"" +
+                " --validate --calcDecimals --logLevel=error --logLevel=assertion-not-satisfied --logLevel=inconsistency --logCodeFilter=^((?!.*arelleUtrLoader).)*$ --logFile " +
+                "\"{INSTANCENAME}-arelle.log.xml\"";
+
+            string fileName = instancePath + instanceName;
+
+            var argumentParse = arelleArgument.Replace("{INSTANCENAME}", fileName);
+
+            CmdLineUtility.ExceuteCommand(arellePath, argumentParse);
+            //if (args.Length > 0)
+            //{
+            //    var arg = args[0];
+            //    if (arg == null) return;
+
+            //    if (arg.ToLower().Contains("--cmdpath="))
+            //    {
+            //        var command = arg.ToLower().Replace("--cmdpath=", "");
+            //        CmdLineUtility.ExceuteCommand(command);
+            //    }
+            //}
+        }
+    }
+}
---
+++ ArelleValidation.CmdLine/Utilities/CmdLineUtility.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArelleValidation.CmdLine.Utilities
+{
+    public class CmdLineUtility
+    {
+        public static void ExceuteCommand(string command)
+        {
+            var process = new Process();
+            process.StartInfo = new ProcessStartInfo(command)
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true
+            };
+            process.Start();
+            process.WaitForExit();
+        }
+        public static void ExceuteCommand(string executableName, string argument)
+        {
+            var StartInfo = new ProcessStartInfo()
+            {
+                FileName = executableName,
+                Arguments = argument,
+                CreateNoWindow = false,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+
+            var proc = Process.Start(StartInfo);
+            proc.WaitForExit();
+            proc.Close();
+        }
+    }
+}
